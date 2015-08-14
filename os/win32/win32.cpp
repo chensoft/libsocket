a171a580577ad823311a1a1a5a97abefd98f5036
chenjian <chenjian@xindong.com>
2015-08-14 16:46:43 +0800

    os: distinguish unix & win32

---
+++ os/win32/win32.cpp
@@ -0,0 +1,9 @@
+/**
+ * Created by Jian Chen
+ * @since  2015.08.14
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://www.chensoft.com
+ */
+#ifdef CHEN_OS_WIN32
+    #pragma message ( "Detect Windows" )
+#endif
