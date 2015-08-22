5e0dedf24c475937fd95436b151595819523cfb9
chenjian <chenjian@xindong.com>
2015-08-22 14:07:02 +0800

    udp: add udp client and server file

---
+++ net/udp/udp_client.cpp
@@ -0,0 +1,13 @@
+/**
+ * Created by Jian Chen
+ * @since  2015.08.22
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://www.chensoft.com
+ */
+#include "udp_client.h"
+
+using namespace chen;
+using namespace chen::udp;
+
+// -----------------------------------------------------------------------------
+// client
---
+++ net/udp/udp_server.h
@@ -0,0 +1,18 @@
+/**
+ * Created by Jian Chen
+ * @since  2015.08.22
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://www.chensoft.com
+ */
+#pragma once
+
+namespace chen
+{
+    namespace udp
+    {
+        class server
+        {
+
+        };
+    }
+}
