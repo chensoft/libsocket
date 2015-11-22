74f0b72d9df49de56e644d638b8f523e856bdb5d
chenjian <chenjian@xindong.com>
2015-11-22 16:23:24 +0800

    udp: add client class

---
+++ chen/net/udp/udp_client.cpp
@@ -0,0 +1,17 @@
+/**
+ * Created by Jian Chen
+ * @since  2015.11.22
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://www.chensoft.com
+ */
+#include "udp_client.h"
+
+using namespace chen;
+using namespace chen::udp;
+
+// -----------------------------------------------------------------------------
+// client
+client::client()
+{
+
+}
---
+++ chen/net/udp/udp_client.h
@@ -0,0 +1,21 @@
+/**
+ * Created by Jian Chen
+ * @since  2015.11.22
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://www.chensoft.com
+ */
+#pragma once
+
+#include "udp_socket.h"
+
+namespace chen
+{
+    namespace udp
+    {
+        class client : public socket
+        {
+        public:
+            client();
+        };
+    }
+}
