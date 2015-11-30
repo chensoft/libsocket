096fc607354dc38596b41fa4f3833b5a047268af
chenjian <chenjian@xindong.com>
2015-11-30 20:21:36 +0800

    dns: add message, request and response

--- chen/net/dns/dns_packet.cpp
+++ chen/net/dns/dns_packet.cpp
@@ -6,3 +6,121 @@
  */
 #include "dns_packet.h"
 
+using namespace chen;
+using namespace chen::dns;
+
+// -----------------------------------------------------------------------------
+// message
+
+
+// -----------------------------------------------------------------------------
+// request
+request::request()
+{
+    // use random id
+    this->_header.setId();
+
+    // set query
+    this->_header.setQr(QR::Query);
+}
+
+// field value
+chen::dns::header request::header() const
+{
+    return this->_header;
+}
+
+chen::dns::question request::question() const
+{
+    return this->_question;
+}
+
+// set query
+void request::setQuery(const std::string &qname,
+                       chen::dns::RRType qtype,
+                       chen::dns::RRClass qclass)
+{
+    // todo @@
+//    // check empty
+//    if (qname.empty())
+//        throw error_size("request query name is empty");
+//
+//    // check fqdn
+//    if (!tool::isFqdn(qname))
+//        throw error_fqdn("request query name is not fqdn");
+//
+//    // check total length
+//    if (qname.size() > 255)
+//        throw error_size("request query name length must be 255 or less");
+//
+//    // check each label
+//    std::string::size_type count = 0;
+//
+//    for (std::uint8_t i = 0, len = static_cast<std::uint8_t>(qname.size()); i < len; ++i)
+//    {
+//        char c = qname[i];
+//
+//        if (c == '.')
+//        {
+//            if (!count)
+//                throw error_size("request query label is empty");
+//
+//            count = 0;
+//        }
+//        else
+//        {
+//            ++count;
+//
+//            if (count > 63)
+//                throw error_size("request query label length must be 63 or less");
+//        }
+//    }
+//
+//    // set opcode
+//    this->_header.setOpcode(chen::dns::OPCODE::Query);
+//
+//    // set qdcount
+//    this->_header.setQdcount(1);
+//
+//    // set query
+//    this->_question.setQname(qname);
+//    this->_question.setQtype(qtype);
+//    this->_question.setQclass(qclass);
+}
+
+// flag
+void request::setRecursionDesired()
+{
+    this->_header.setRecursionDesired(true);
+}
+
+
+// -----------------------------------------------------------------------------
+// response
+
+// header
+chen::dns::header& response::header()
+{
+    return this->_header;
+}
+
+// rrs
+response::q_type& response::question()
+{
+    return this->_question;
+}
+
+response::rr_type& response::answer()
+{
+    return this->_answer;
+}
+
+response::rr_type& response::authority()
+{
+    return this->_authority;
+}
+
+response::rr_type& response::additional()
+{
+    return this->_additional;
+}
--- chen/net/dns/dns_packet.h
+++ chen/net/dns/dns_packet.h
@@ -6,10 +6,85 @@
  */
 #pragma once
 
+#include "dns_header.h"
+#include "dns_record.h"
+#include <vector>
+
 namespace chen
 {
     namespace dns
     {
+        // ---------------------------------------------------------------------
+        // message
+        class message
+        {
+        public:
+            virtual ~message() = default;
+        };
 
+
+        // ---------------------------------------------------------------------
+        // request
+        class request : public message
+        {
+        public:
+            request();
+
+        public:
+            /**
+             * Get field value
+             */
+            chen::dns::header header()     const;
+            chen::dns::question question() const;
+
+        public:
+            /**
+             * Set query data
+             */
+            void setQuery(const std::string &qname,
+                          chen::dns::RRType qtype,
+                          chen::dns::RRClass qclass = chen::dns::RRClass::IN);
+
+            /**
+             * Enable RD flag, default is true
+             */
+            void setRecursionDesired();
+
+        private:
+            chen::dns::header _header;
+            chen::dns::question _question;
+        };
+
+
+        // ---------------------------------------------------------------------
+        // response
+        class response : public message
+        {
+        public:
+            typedef std::vector<chen::dns::question> q_type;
+            typedef std::vector<std::shared_ptr<chen::dns::RR>> rr_type;
+
+        public:
+            /**
+             * Header
+             */
+            chen::dns::header& header();
+
+            /**
+             * Four RRs
+             */
+            q_type& question();
+            rr_type& answer();
+            rr_type& authority();
+            rr_type& additional();
+
+        private:
+            chen::dns::header _header;
+
+            q_type  _question;
+            rr_type _answer;
+            rr_type _authority;
+            rr_type _additional;
+        };
     }
 }
