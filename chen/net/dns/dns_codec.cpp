1d19b0b5d09bb51a972438f71afef15ce111860e
chenjian <chenjian@xindong.com>
2015-12-13 21:19:48 +0800

    codec: add decoding method

--- chen/net/dns/dns_codec.cpp
+++ chen/net/dns/dns_codec.cpp
@@ -51,7 +51,7 @@ void codec::clear()
     this->_map.clear();
 }
 
-// data
+// data pack
 void codec::pack(std::int8_t value, std::vector<std::uint8_t> &store)
 {
     codec::pack(static_cast<std::uint8_t>(value), store);
@@ -162,6 +162,124 @@ void codec::pack(const std::string &value, bool domain, std::vector<std::uint8_t
         store.push_back(static_cast<std::uint8_t>(value.size()));
         store.insert(store.end(), value.cbegin(), value.cend());
     }
+}
+
+// data unpack
+std::size_t codec::unpack(std::int8_t &value, const std::uint8_t *data, std::size_t size)
+{
+    codec::check(1, size, "codec unpack size is not enough, require 1 byte");
+    value = static_cast<std::int8_t>(data[0]);
+    return 1;
+}
+
+std::size_t codec::unpack(std::int16_t &value, const std::uint8_t *data, std::size_t size)
+{
+    codec::check(2, size, "codec unpack size is not enough, require 2 bytes");
+    value = static_cast<std::int16_t>((data[0] << 8) + data[1]);
+    return 2;
+}
+
+std::size_t codec::unpack(std::int32_t &value, const std::uint8_t *data, std::size_t size)
+{
+    codec::check(4, size, "codec unpack size is not enough, require 4 bytes");
+    value = static_cast<std::int32_t>((data[0] << 24) + (data[1] << 16) + (data[2] << 8) + data[3]);
+    return 4;
+}
+
+std::size_t codec::unpack(std::uint8_t &value, const std::uint8_t *data, std::size_t size)
+{
+    codec::check(1, size, "codec unpack size is not enough, require 1 byte");
+    value = data[0];
+    return 1;
+}
+
+std::size_t codec::unpack(std::uint16_t &value, const std::uint8_t *data, std::size_t size)
+{
+    codec::check(2, size, "codec unpack size is not enough, require 2 bytes");
+    value = static_cast<std::uint16_t>((data[0] << 8) + data[1]);
+    return 2;
+}
+
+std::size_t codec::unpack(std::uint32_t &value, const std::uint8_t *data, std::size_t size)
+{
+    codec::check(4, size, "codec unpack size is not enough, require 4 bytes");
+    value = static_cast<std::uint32_t>((data[0] << 24) + (data[1] << 16) + (data[2] << 8) + data[3]);
+    return 4;
+}
+
+std::size_t codec::unpack(chen::dns::RRType &value, const std::uint8_t *data, std::size_t size)
+{
+    codec::check(2, size, "codec unpack size is not enough, require 2 bytes");
+    value = static_cast<chen::dns::RRType>((data[0] << 8) + data[1]);
+    return 2;
+}
+
+std::size_t codec::unpack(chen::dns::RRClass &value, const std::uint8_t *data, std::size_t size)
+{
+    codec::check(2, size, "codec unpack size is not enough, require 2 bytes");
+    value = static_cast<chen::dns::RRClass>((data[0] << 8) + data[1]);
+    return 2;
+}
+
+std::size_t codec::unpack(std::string &value, bool domain, const std::uint8_t *data, std::size_t size)
+{
+    codec::check(1, size, "codec unpack size is zero");
+
+    std::size_t total = 0;
+
+    if (domain)
+    {
+        // Note:
+        // value contains multiple labels, each label is split by a dot
+        // one byte label length + label characters + ... + one byte ending
+        std::size_t origin = value.size();
+        std::size_t length = 0;
+
+        try
+        {
+            while (*data)
+            {
+                length = static_cast<std::size_t>(data[0]) + 1;
+                codec::check(length, size, str::format("codec unpack domain size is not enough, require %d bytes", length));
+
+                // check limit
+                if (length > SIZE_LIMIT_LABEL)
+                    throw error_size(str::format("codec unpack domain label must be %d octets or less", SIZE_LIMIT_LABEL));
+
+                for (std::size_t i = 0, len = length - 1; i < len; ++i)
+                {
+                    value += data[i + 1];
+                }
+
+                data  += length;
+                value += ".";
+            }
+        }
+        catch (...)
+        {
+            value.erase(origin, value.size() - origin);
+            throw;
+        }
+    }
+    else
+    {
+        // Note:
+        // value is plain text
+        // one byte length + characters
+        total = static_cast<std::size_t>(data[0]) + 1;
+        codec::check(total, size, str::format("codec unpack string size is not enough, require %d bytes", total));
+
+        value.insert(value.size(), reinterpret_cast<const char*>(data), total);
+    }
+
+    return total;
+}
+
+// check
+void codec::check(std::size_t require, std::size_t size, const std::string &msg)
+{
+    if (size < require)
+        throw error_size(msg);
 }
 
 // fqdn
--- chen/net/dns/dns_codec.h
+++ chen/net/dns/dns_codec.h
@@ -47,6 +47,22 @@ namespace chen
             static void pack(chen::dns::RRClass value, std::vector<std::uint8_t> &store);
             static void pack(const std::string &value, bool domain, std::vector<std::uint8_t> &store);
 
+            static std::size_t unpack(std::int8_t &value, const std::uint8_t *data, std::size_t size);
+            static std::size_t unpack(std::int16_t &value, const std::uint8_t *data, std::size_t size);
+            static std::size_t unpack(std::int32_t &value, const std::uint8_t *data, std::size_t size);
+            static std::size_t unpack(std::uint8_t &value, const std::uint8_t *data, std::size_t size);
+            static std::size_t unpack(std::uint16_t &value, const std::uint8_t *data, std::size_t size);
+            static std::size_t unpack(std::uint32_t &value, const std::uint8_t *data, std::size_t size);
+            static std::size_t unpack(chen::dns::RRType &value, const std::uint8_t *data, std::size_t size);
+            static std::size_t unpack(chen::dns::RRClass &value, const std::uint8_t *data, std::size_t size);
+            static std::size_t unpack(std::string &value, bool domain, const std::uint8_t *data, std::size_t size);
+
+        private:
+            /**
+             * Check buffer size
+             */
+            static void check(std::size_t require, std::size_t size, const std::string &msg);
+
         public:
             /**
              * Fully qualified domain name detection
