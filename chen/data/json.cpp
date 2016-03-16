c42834744f9e602fda49a921dbaeab5564612c16
chenjian <chenjian@xindong.com>
2016-03-16 20:13:38 +0800

    json: add basic interface

--- chen/data/json.cpp
+++ chen/data/json.cpp
@@ -1,16 +1,308 @@
 /**
  * Created by Jian Chen
- * @since  2015.07.30
+ * @since  2016.03.16
  * @author Jian Chen <admin@chensoft.com>
  * @link   http://chensoft.com
  */
 #include "json.hpp"
+#include <cstdlib>
+#include <chen/tool/num.hpp>
 
 using namespace chen;
 
 // -----------------------------------------------------------------------------
 // json
-json::json()
+json::json(const json &o)
+: _type(o._type)
+, _data(o._data)
 {
 
+}
+
+json::json(json &&o)
+: _type(o._type)
+, _data(o._data)
+{
+
+}
+
+json::json(const chen::json::object &v)
+: _type(JsonType::Object)
+, _data(v)
+{
+
+}
+
+json::json(chen::json::object &&v)
+: _type(JsonType::Object)
+, _data(chen::json::object(v))
+{
+
+}
+
+json::json(const chen::json::array &v)
+: _type(JsonType::Array)
+, _data(v)
+{
+
+}
+
+json::json(chen::json::array &&v)
+: _type(JsonType::Array)
+, _data(chen::json::array(v))
+{
+
+}
+
+json::json(double v)
+: _type(JsonType::Number)
+, _data(v)
+{
+
+}
+
+json::json(int v)
+: _type(JsonType::Number)
+, _data(static_cast<double>(v))
+{
+
+}
+
+json::json(const std::string &v)
+: _type(JsonType::String)
+, _data(std::string(v))
+{
+
+}
+
+json::json(std::string &&v)
+: _type(JsonType::String)
+, _data(std::string(v))
+{
+
+}
+
+json::json(const char *v)
+: _type(JsonType::String)
+, _data(std::string(v))
+{
+
+}
+
+json::json(bool v)
+: _type(v ? JsonType::True : JsonType::False)
+, _data(v)
+{
+
+}
+
+// parse
+void json::parse(const std::string &text)
+{
+
+}
+
+// clear
+void json::clear()
+{
+    this->_type = JsonType::Null;
+    this->_data.clear();
+}
+
+// type
+chen::json::JsonType json::type() const
+{
+    return this->_type;
+}
+
+// check
+bool json::isObject() const
+{
+    return this->_type == JsonType::Object;
+}
+
+bool json::isArray() const
+{
+    return this->_type == JsonType::Array;
+}
+
+bool json::isNumber() const
+{
+    return this->_type == JsonType::Number;
+}
+
+bool json::isString() const
+{
+    return this->_type == JsonType::String;
+}
+
+bool json::isTrue() const
+{
+    return this->_type == JsonType::True;
+}
+
+bool json::isFalse() const
+{
+    return this->_type == JsonType::False;
+}
+
+bool json::isNull() const
+{
+    return this->_type == JsonType::Null;
+}
+
+bool json::isBool() const
+{
+    return (this->_type == JsonType::True) || (this->_type == JsonType::False);
+}
+
+// value
+chen::json::object json::asObject() const
+{
+    static chen::json::object object;
+
+    switch (this->_type)
+    {
+        case JsonType::Object:
+            return this->_data;
+
+        case JsonType::Array:
+        case JsonType::Number:
+        case JsonType::String:
+        case JsonType::True:
+        case JsonType::False:
+        case JsonType::Null:
+            return object;
+    }
+}
+
+chen::json::array json::asArray() const
+{
+    static chen::json::array array;
+
+    switch (this->_type)
+    {
+        case JsonType::Object:
+            return array;
+
+        case JsonType::Array:
+            return this->_data;
+
+        case JsonType::Number:
+        case JsonType::String:
+        case JsonType::True:
+        case JsonType::False:
+        case JsonType::Null:
+            return array;
+    }
+}
+
+double json::asNumber() const
+{
+    switch (this->_type)
+    {
+        case JsonType::Object:
+        case JsonType::Array:
+            return 0.0;
+
+        case JsonType::Number:
+            return static_cast<double>(this->_data);
+
+        case JsonType::String:
+        {
+            std::string d = this->_data;
+            return std::atof(d.c_str());
+        }
+
+        case JsonType::True:
+            return 1.0;
+
+        case JsonType::False:
+        case JsonType::Null:
+            return 0.0;
+    }
+}
+
+std::string json::asString() const
+{
+    switch (this->_type)
+    {
+        case JsonType::Object:
+        case JsonType::Array:
+            return "";
+
+        case JsonType::Number:
+        {
+            // todo check if prevent extra zeros
+            double d = static_cast<double>(this->_data);
+            std::int64_t i = static_cast<std::int64_t>(d);
+            return (d - i == 0) ? num::str(i) : num::str(d);
+        }
+
+        case JsonType::String:
+            return this->_data;
+
+        case JsonType::True:
+            return "true";
+
+        case JsonType::False:
+            return "false";
+
+        case JsonType::Null:
+            return "null";
+    }
+}
+
+bool json::asBool() const
+{
+    switch (this->_type)
+    {
+        // treat object as true, like javascript
+        case JsonType::Object:
+        case JsonType::Array:
+            return true;
+
+        case JsonType::Number:
+            return static_cast<double>(this->_data) != 0.0;
+
+        case JsonType::String:
+        {
+            std::string d = this->_data;
+            return !d.empty();
+        }
+
+        case JsonType::True:
+            return true;
+
+        case JsonType::False:
+        case JsonType::Null:
+            return false;
+    }
+}
+
+// copy & move
+json& json::operator=(const json &o)
+{
+    if (this == &o)
+        return *this;
+
+    this->clear();
+
+    this->_type = o._type;
+    this->_data = o._data;
+
+    return *this;
+}
+
+json& json::operator=(json &&o)
+{
+    if (this == &o)
+        return *this;
+
+    this->clear();
+
+    this->_type = o._type;
+    this->_data = o._data;
+
+    return *this;
 }
--- chen/data/json.hpp
+++ chen/data/json.hpp
@@ -1,21 +1,121 @@
 /**
- * Created by Jian Chen
- * @since  2015.07.30
+ * A tiny json parser
+ * @since  2016.03.16
  * @author Jian Chen <admin@chensoft.com>
  * @link   http://chensoft.com
+ * @link   http://www.json.org
+ * -----------------------------------------------------------------------------
+ * A JSON value can be an object, array, number, string, true, false, or null
+ * we represent the number as double type, like the javascript
+ * -----------------------------------------------------------------------------
+ * Usage:  // todo
+ * >> chen::cmd cmd;
+ * >>
+ * >> try
+ * >> {
+ * >>     cmd.define("help", "h", "show help", false);
+ * >>     cmd.define("port", "p", "server port (default: 53)", 53);
+ * >>     cmd.define("zone", "z", "zone folder", "");
+ * >>
+ * >>     cmd.parse(argc, argv);
+ * >>
+ * >>     bool help = cmd.boolVal("help");
+ * >> }
+ * >> catch (const chen::cmd::error_parse &e)
+ * >> {
+ * >>     std::cout << cmd.usage(e) << std::endl;
+ * >> }
  */
 #pragma once
 
-// todo no implement now
+#include <string>
+#include <vector>
+#include <map>
+#include <chen/tool/any.hpp>
 
 namespace chen
 {
     class json
     {
     public:
-        json();
+        typedef std::vector<chen::json> array;
+        typedef std::map<std::string, chen::json> object;
+
+        enum class JsonType {Object, Array, Number, String, True, False, Null};
 
     public:
+        json() = default;
 
+        json(const json &o);
+        json(json &&o);
+
+        json(const chen::json::object &v);
+        json(chen::json::object &&v);
+
+        json(const chen::json::array &v);
+        json(chen::json::array &&v);
+
+        json(double v);
+        json(int v);
+
+        json(const std::string &v);
+        json(std::string &&v);
+        json(const char *v);
+
+        json(bool v);
+
+        virtual ~json() = default;
+
+    public:
+        /**
+         * Parse the json text, throw exception if found error
+         */
+        virtual void parse(const std::string &text);
+
+        /**
+         * Clear the internal state
+         */
+        virtual void clear();
+
+    public:
+        /**
+         * Get the json type
+         */
+        virtual chen::json::JsonType type() const;
+
+        /**
+         * Check the json type
+         */
+        virtual bool isObject() const;
+        virtual bool isArray() const;
+        virtual bool isNumber() const;
+        virtual bool isString() const;
+        virtual bool isTrue() const;
+        virtual bool isFalse() const;
+        virtual bool isNull() const;
+
+        virtual bool isBool() const;
+
+    public:
+        /**
+         * Get the json value
+         * convert to the desired type as possible
+         */
+        virtual chen::json::object asObject() const;
+        virtual chen::json::array asArray() const;
+        virtual double asNumber() const;
+        virtual std::string asString() const;
+        virtual bool asBool() const;
+
+    public:
+        /**
+         * Copy & Move
+         */
+        json& operator=(const json &o);
+        json& operator=(json &&o);
+
+    protected:
+        JsonType _type = JsonType::Null;
+        chen::any _data;
     };
 }
