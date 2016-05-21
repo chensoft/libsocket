12b2a3f99838d963c5376bee4f90ca15caee1c66
Jian Chen <admin@chensoft.com>
2016-05-21 08:47:02 +0800

    unit: using Google Test for unit testing

---
+++ unit/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * Created by Jian Chen
+ * @since  2016.05.20
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://chensoft.com
+ */
+#include <chen/chen.hpp>
+#include <gtest/gtest.h>
+
+#include "net.ip.hpp"
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
