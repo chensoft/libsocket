ded19f39b5a2694a6d7dc869e42180152129daf1
Jian Chen <admin@chensoft.com>
2016-06-06 13:36:31 +0800

    test: compile regex unit test on gcc 4.9+

--- test/src/base/regex.hpp
+++ test/src/base/regex.hpp
@@ -12,13 +12,20 @@
 TEST(BaseRegexTest, General)
 {
     // regex support is incomplete in gcc 4.8.x, so I comment out these codes
-//    EXPECT_TRUE(chen::regex::match("12345", "^\\d+$"));
-//    EXPECT_EQ("chenjian", chen::regex::replace("chensoft.com", "soft\\.com", "jian"));
-//
-//    auto group = chen::regex::group("127.0.0.1", "(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)");
-//    EXPECT_EQ(5, group.size());
-//    EXPECT_EQ("127", group[1]);
-//    EXPECT_EQ("0", group[2]);
-//    EXPECT_EQ("0", group[3]);
-//    EXPECT_EQ("1", group[4]);
+#if defined(__GNUC__) && !defined(__clang__)
+#if __GNUC__ <= 4 && __GNUC_MINOR__ <= 8
+    PILogD("test: regex support is incomplete below gcc 4.8, skip it");
+    return;
+#endif
+#endif
+
+    EXPECT_TRUE(chen::regex::match("12345", "^\\d+$"));
+    EXPECT_EQ("chenjian", chen::regex::replace("chensoft.com", "soft\\.com", "jian"));
+
+    auto group = chen::regex::group("127.0.0.1", "(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)");
+    EXPECT_EQ(5, group.size());
+    EXPECT_EQ("127", group[1]);
+    EXPECT_EQ("0", group[2]);
+    EXPECT_EQ("0", group[3]);
+    EXPECT_EQ("1", group[4]);
 }
