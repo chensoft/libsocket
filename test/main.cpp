181507f46e0f91942bb1ec7fedd3f03086a6f480
Jian Chen <admin@chensoft.com>
2016-06-14 18:36:27 +0800

    utf8: remove unit test, not compatible with vs2015

--- test/main.cpp
+++ test/main.cpp
@@ -4,16 +4,11 @@
  * @author Jian Chen <admin@chensoft.com>
  * @link   http://chensoft.com
  */
-#ifdef _MSC_VER
-#pragma warning(disable:4819)
-#endif
-
 #include "src/base/any.hpp"
 #include "src/base/map.hpp"
 #include "src/base/num.hpp"
 #include "src/base/regex.hpp"
 #include "src/base/str.hpp"
-#include "src/base/utf8.hpp"
 #include "src/base/vector.hpp"
 
 #include "src/sys/fs.hpp"
