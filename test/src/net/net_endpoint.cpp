f19767e109736005746499eb2a5330b91081afea
Jian Chen <admin@chensoft.com>
2016-09-23 22:39:11 +0800

    test: split same unit test to more classification

--- test/src/net/net_endpoint.cpp
+++ test/src/net/net_endpoint.cpp
@@ -9,18 +9,18 @@
 
 TEST(NetEndpointTest, General)
 {
-    using chen::ip::address;
     using chen::net::endpoint;
 
     // construct
     EXPECT_TRUE(endpoint(nullptr).empty());
     EXPECT_FALSE(endpoint(nullptr));
 
-    EXPECT_EQ("127.0.0.1:80", endpoint("127.0.0.1", 80).str());
     EXPECT_EQ("0.0.0.0:80", endpoint(":80").str());
+    EXPECT_EQ("127.0.0.1:80", endpoint("127.0.0.1", 80).str());
     EXPECT_EQ("127.0.0.1:0", endpoint("127.0.0.1").str());
     EXPECT_EQ("127.0.0.1:80", endpoint("127.0.0.1:80").str());
     EXPECT_EQ("127.0.0.1:80", endpoint("127.0.0.1:http").str());
+
     EXPECT_EQ("[::]:80", endpoint("[::]:80").str());
     EXPECT_EQ("[fe80::1]:0", endpoint("[fe80::1]").str());
     EXPECT_EQ("[fe80::1]:80", endpoint("[fe80::1]:80").str());
@@ -28,6 +28,12 @@ TEST(NetEndpointTest, General)
     EXPECT_EQ("[fe80::1]:0", endpoint("[fe80::1%lo0]").str());
     EXPECT_EQ("[fe80::1]:80", endpoint("[fe80::1%lo0]:80").str());
     EXPECT_EQ("[fe80::1]:80", endpoint("[fe80::1%lo0]:http").str());
+}
+
+TEST(NetEndpointTest, Property)
+{
+    using chen::net::endpoint;
+    using chen::ip::address;
 
     // modify port and address
     endpoint ep("127.0.0.1", 80);
@@ -44,6 +50,11 @@ TEST(NetEndpointTest, General)
     // address with port
     EXPECT_EQ("127.0.0.1:80", endpoint("127.0.0.1", 80).str());
     EXPECT_EQ("[::1]:80", endpoint("::1", 80).str());
+}
+
+TEST(NetEndpointTest, Special)
+{
+    using chen::net::endpoint;
 
     // special ports
     EXPECT_TRUE(endpoint("127.0.0.1", 0).isWellKnownPort());
@@ -60,8 +71,13 @@ TEST(NetEndpointTest, General)
     EXPECT_TRUE(endpoint("127.0.0.1", 49152).isDynamicPort());
     EXPECT_TRUE(endpoint("127.0.0.1", 50000).isDynamicPort());
     EXPECT_TRUE(endpoint("127.0.0.1", 65535).isDynamicPort());
+}
 
-    // comparision
+TEST(NetEndpointTest, Compare)
+{
+    using chen::net::endpoint;
+
+    // compare
     EXPECT_EQ(endpoint("127.0.0.1", 80), endpoint("127.0.0.1", 80));
     EXPECT_NE(endpoint("192.168.0.1", 80), endpoint("127.0.0.1", 80));
 
