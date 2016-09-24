66f47624e2aadf523cf48e47779cf2f09192036a
Jian Chen <admin@chensoft.com>
2016-09-24 10:04:20 +0800

    option: placeholder for udp option

---
+++ include/socket/udp/udp_option.hpp
@@ -0,0 +1,21 @@
+/**
+ * Created by Jian Chen
+ * @since  2016.09.02
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://chensoft.com
+ */
+#pragma once
+
+#include <socket/ip/ip_option.hpp>
+
+namespace chen
+{
+    namespace udp
+    {
+        class option : public ip::option
+        {
+        public:
+            option(socket_t fd);
+        };
+    }
+}
---
+++ src/udp/udp_option.cpp
@@ -0,0 +1,13 @@
+/**
+ * Created by Jian Chen
+ * @since  2016.09.02
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://chensoft.com
+ */
+#include <socket/udp/udp_option.hpp>
+
+// -----------------------------------------------------------------------------
+// option
+chen::udp::option::option(socket_t fd) : ip::option(fd)
+{
+}
