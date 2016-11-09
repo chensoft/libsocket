298bfd3177f643ce628c199e1633e7e597d5e2c9
Jian Chen <admin@chensoft.com>
2016-11-09 15:56:20 +0800

    dgram: add client

--- include/socket/base/basic_socket.hpp
+++ include/socket/base/basic_socket.hpp
@@ -10,24 +10,47 @@
 
 namespace chen
 {
+    /**
+     * The basic class for dgram_socket, stream_socket and more
+     */
     template <typename T>
     class basic_socket
     {
     public:
+        /**
+         * Requirements for address:
+         * -:) must have a constructor which accept bsd_address as its param
+         * -:) must define bsd_address conversion operator, see inet_address
+         */
         typedef typename T::address_type address_type;
+
+        /**
+         * Requirements for option:
+         * -:) must derived from bsd_option and accept bsd_socket& as its param
+         */
         typedef typename T::option_type option_type;
 
     public:
         /**
          * Peer & Local address
          */
-        address_type peer() const noexcept;
-        address_type sock() const noexcept;
+        address_type peer() const noexcept
+        {
+            return this->_socket.peer();
+        }
+
+        address_type sock() const noexcept
+        {
+            return this->_socket.sock();
+        }
 
         /**
          * Non-blocking mode
          */
-        std::error_code nonblocking(bool enable) noexcept;
+        std::error_code nonblocking(bool enable) noexcept
+        {
+            return this->_socket.nonblocking(enable);
+        }
 
         /**
          * Get/Set socket option
@@ -36,35 +59,60 @@ namespace chen
          * >> opt.reuseaddr(true);           // set option
          * >> bool reuse = opt.reuseaddr();  // get option
          */
-        option_type option() noexcept;
+        option_type option() noexcept
+        {
+            return option_type(*this);
+        }
 
         /**
          * Check socket is valid
          * @attention zero is a valid file descriptor
          */
-        bool valid() const noexcept;
-        operator bool() const noexcept;
+        bool valid() const noexcept
+        {
+            return this->_socket.valid();
+        }
+
+        operator bool() const noexcept
+        {
+            return this->_socket;
+        }
 
         /**
          * Native socket handle
          */
-        socket_t native() const noexcept;
+        socket_t native() const noexcept
+        {
+            return this->_socket.native();
+        }
 
         /**
          * Available bytes to read without blocking
          */
-        std::size_t available() const noexcept;
+        std::size_t available() const noexcept
+        {
+            return this->_socket.available();
+        }
 
         /**
          * Native type info
          */
-        int family() const noexcept;
-        int type() const noexcept;
-        int protocol() const noexcept;
+        int family() const noexcept
+        {
+            return this->_socket.family();
+        }
+
+        int type() const noexcept
+        {
+            return this->_socket.type();
+        }
+
+        int protocol() const noexcept
+        {
+            return this->_socket.protocol();
+        }
 
     protected:
         bsd_socket _socket;
     };
 }
-
-#include <socket/base/basic_socket.ipp>
---
+++ include/socket/base/dgram_client.hpp
@@ -0,0 +1,28 @@
+/**
+ * Created by Jian Chen
+ * @since  2016.11.09
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://chensoft.com
+ */
+#pragma once
+
+#include <socket/base/dgram_socket.hpp>
+
+namespace chen
+{
+    template <typename T>
+    class dgram_client : public dgram_socket<T>
+    {
+    public:
+        /**
+         * Datagram client is connectionless, so we init socket here
+         */
+        dgram_client(int family, int protocol = 0)
+        {
+            this->_socket.reset(family, SOCK_DGRAM, protocol);
+        }
+
+    public:
+
+    };
+}
