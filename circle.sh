579ae11c935674af4a33d6030a9321bbdeaefabe
Jian Chen <admin@chensoft.com>
2016-11-20 22:54:06 +0800

    ci: rename folder

--- circle.sh
+++ circle.sh
@@ -3,13 +3,13 @@
 set -e
 
 # compile
-mkdir test/build
-cd test/build
+mkdir build
+cd build
 cmake -DLIBSOCKET_TEST_CODE_COVERAGE=ON ..
-make
+make -j4
 
 # test
-./libsocket_test
+test/libsocket_test
 
 # code coverage
 bash <(curl -s https://codecov.io/bash) -t 647d3dc9-cc6e-4d24-8cc4-66764a63527c
