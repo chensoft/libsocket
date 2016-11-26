27f079e5bce41fb0a6f6963985fca1ac4576aad1
Jian Chen <admin@chensoft.com>
2016-11-26 11:15:35 +0800

    codecov: try to restore coverage

--- circle.sh
+++ circle.sh
@@ -9,7 +9,8 @@ cmake -DLIBSOCKET_TEST_CODE_COVERAGE=ON ..
 make -j4
 
 # test
-test/libsocket_test
+cd test
+./libsocket_test
 
 # code coverage
 bash <(curl -s https://codecov.io/bash) -t 647d3dc9-cc6e-4d24-8cc4-66764a63527c
